04331f613d2332285fa6861fa3bfca5e29182070
Zach Dennis <zach.dennis@gmail.com>
2014-02-15 18:24:07 -0500

    Updating Door#open to look for a DOOR_OPEN_COMMAND environment variable which it will use to open the door.

--- src/door.js
+++ src/door.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
 
-var debug = require('debug')('door');
+var debug       = require('debug')('door'),
+    exec        = require('child_process').exec,
+    fs          = require('fs'),    
+    doorOpenCommand = process.env.DOOR_OPEN_COMMAND;
+
+if(!doorOpenCommand)
+  throw("The DOOR_OPEN_COMMAND environment variable wasn't specified. It must be supplied and point to an executable to open the door.");
+
+if(!fs.existsSync(doorOpenCommand))
+  throw("The door open command doesn't exist! Please check your path, was given: DOOR_OPEN_COMMAND=" + allowedFile);
+
+debug("Using door open command: " + doorOpenCommand);
+
 
 var Door = {
   open: function(){
-    debug("Door#open with arguments: ", arguments);
+    var child;
+
+    child = exec(doorOpenCommand, function (error, stdout, stderr) {
+      debug("Door open stdout: " + stdout);
+      debug("Door open stderr: " + stderr);
+      if (error !== null) {
+        console.log('Door open error: ' + error);
+      }
+    });
   }
 };
 

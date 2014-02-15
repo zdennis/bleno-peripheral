9220241ee40f6ef1ede6dd525d74f872a6ff17c6
Zach Dennis <zach.dennis@gmail.com>
2014-02-15 18:15:28 -0500

    Updating Authentication to rely on a JSON file which uses tokens as the keys to verify if a given token is allowed to open the door.
    Uses environment variable:
        ALLOWED_FILE=/path/to/allowed.json

--- src/authentication.js
+++ src/authentication.js
@@ -1,10 +1,21 @@
-var debug = require('debug')('door');
+var debug       = require('debug')('door'),
+    fs          = require('fs'),
+    allowedFile = process.env.ALLOWED_FILE;
 
+if(!allowedFile)
+  throw("The allowed file wasn't specified. It must be supplied as an environment variable, e.g: ALLOWED_FILE=/path/to/file.json");
+
+if(!fs.existsSync(allowedFile))
+  throw("The allowed file doesn't exist! Please check your path, was given: ALLOWED_FILE=" + allowedFile);
+
+debug("Using allowed file at " + allowedFile);
 
 var Authentication = {
   authenticate: function(secret){
+    var allowed = JSON.parse(fs.readFileSync(allowedFile));
+
     debug('Authentication#authenticate with arguments: ', arguments);
-    return secret === "abc123";
+    return !!allowed[secret];
   }
 };
 
